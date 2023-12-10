cf934e33123edbf1934ad27f92221e5ed468f2bf
esatpiskin <wraithrayy@protonmail.com>
2023-12-10 17:58:16 +0300

    Payment history page refactored

--- app/src/main/java/com/example/myapplication/PaymentHistoryActivity.kt
+++ app/src/main/java/com/example/myapplication/PaymentHistoryActivity.kt
@@ -1,92 +1,86 @@
-// PaymentHistoryActivity.kt
 package com.example.myapplication
 
-import PaymentsAdapter
 import android.os.Bundle
 import android.util.Log
-import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.RecyclerView
 import com.example.myapplication.databinding.ActivityPaymentHistoryBinding
 import com.example.myapplication.model.PaymentDetailsModel
 import com.google.firebase.auth.FirebaseAuth
 import com.google.gson.Gson
 import com.google.gson.reflect.TypeToken
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
-import okhttp3.ResponseBody
-import org.json.JSONObject
+import okhttp3.*
 import java.io.IOException
-import java.util.Date
 
 class PaymentHistoryActivity : AppCompatActivity() {
 
     private lateinit var binding: ActivityPaymentHistoryBinding
+    private lateinit var paymentsAdapter: PaymentsAdapter
+    private var paymentsList = mutableListOf<PaymentDetailsModel>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityPaymentHistoryBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        // Initialize RecyclerView with a LinearLayoutManager and an adapter
-        binding.paymentsRecyclerView.layoutManager = LinearLayoutManager(this)
-        // Here, initialize and set your adapter with the list of payments
-        val paymentsRecyclerView: RecyclerView = findViewById(R.id.paymentsRecyclerView)
-        paymentsRecyclerView.layoutManager = LinearLayoutManager(this)
-
-        val backButton: Button = findViewById(R.id.buttonBack)
-        backButton.setOnClickListener {
-            finish()  // Closes the current activity, returning to the previous one in the stack
+        binding.buttonBack.setOnClickListener {
+            finish() // Return to the previous activity
         }
-        // Send request to backend
+
+        setupRecyclerView()
+        loadPaymentsFromBackend()
+    }
+
+    private fun setupRecyclerView() {
+        paymentsAdapter = PaymentsAdapter(paymentsList) { payment ->
+            requestRefund(payment)
+        }
+        binding.paymentsRecyclerView.apply {
+            layoutManager = LinearLayoutManager(this@PaymentHistoryActivity)
+            adapter = paymentsAdapter
+        }
+    }
+
+    private fun loadPaymentsFromBackend() {
+        // Assume this function retrieves your payments from the backend
         val currentUser = FirebaseAuth.getInstance().currentUser
-        currentUser!!.getIdToken(true).addOnSuccessListener { tokenResult ->
+        currentUser?.getIdToken(true)?.addOnSuccessListener { tokenResult ->
             tokenResult.token?.let { getPayments(currentUser.uid, it) }
         }
-
-    //paymentsRecyclerView.adapter = PaymentsAdapter(getPayments("",""))
     }
-    private fun getPayments(customerId: String, accessToken: String): ResponseBody? {
-        val client = OkHttpClient().newBuilder().build()
+
+    private fun getPayments(customerId: String, accessToken: String) {
+        val client = OkHttpClient()
         val request = Request.Builder()
             .url("http://192.168.128.54:8083/payment/payment-order/customer/$customerId")
             .get()
             .addHeader("Authorization", "Bearer $accessToken")
             .addHeader("Content-Type", "application/json")
             .build()
-        val gson = Gson()
+
         client.newCall(request).enqueue(object : Callback {
             override fun onResponse(call: Call, response: Response) {
-                if (response.isSuccessful) {
-                    val responseBody = response.body
-                    if (responseBody != null) {
-                        val json = responseBody.string()
-                        Log.i("Response:", json)
-
-                        // Parse the JSON into a List<Payment> using Gson
-                        val paymentList: List<PaymentDetailsModel> = gson.fromJson(json, object : TypeToken<List<PaymentDetailsModel>>() {}.type)
-                        runOnUiThread {
-                            val paymentsRecyclerView: RecyclerView = findViewById(R.id.paymentsRecyclerView)
-                            // Check if the RecyclerView and paymentList are not null
-                            paymentsRecyclerView.adapter = paymentList?.let { PaymentsAdapter(it) }
-                        }
-
+                response.body?.let { responseBody ->
+                    val json = responseBody.string()
+                    val gson = Gson()
+                    val paymentListType = object : TypeToken<List<PaymentDetailsModel>>() {}.type
+                    val paymentList: List<PaymentDetailsModel> = gson.fromJson(json, paymentListType)
+                    runOnUiThread {
+                        paymentsList.clear()
+                        paymentsList.addAll(paymentList)
+                        paymentsAdapter.notifyDataSetChanged()
                     }
-                    // Process the responseBody here
-                } else {
-                    Log.e("Error", "Failed to get payments")
-                }
+                } ?: Log.e("Error", "Response body is null")
             }
 
             override fun onFailure(call: Call, e: IOException) {
                 Log.e("Error", "Failed to get payments", e)
             }
         })
+    }
 
-        return null // Replace with the actual return value
+    private fun requestRefund(payment: PaymentDetailsModel) {
+        // Implement refund request logic here
+        Log.i("Refund Request", "Refund requested for: ${payment.description}")
     }
 }
--- app/src/main/java/com/example/myapplication/PaymentsAdapter.kt
+++ app/src/main/java/com/example/myapplication/PaymentsAdapter.kt
@@ -1,31 +1,41 @@
+package com.example.myapplication
+
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
-import com.example.myapplication.PaymentHistoryActivity
-import com.example.myapplication.R
 import com.example.myapplication.model.PaymentDetailsModel
+import com.example.myapplication.R
 
-class PaymentsAdapter(private val payments: List<PaymentDetailsModel>) : RecyclerView.Adapter<PaymentsAdapter.PaymentViewHolder>() {
+class PaymentsAdapter(
+    private val payments: List<PaymentDetailsModel>,
+    private val onRefundRequested: (PaymentDetailsModel) -> Unit
+) : RecyclerView.Adapter<PaymentsAdapter.PaymentViewHolder>() {
 
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PaymentViewHolder {
-        // Inflate the custom layout
         val itemView = LayoutInflater.from(parent.context).inflate(R.layout.item_payment, parent, false)
         return PaymentViewHolder(itemView)
     }
 
     override fun onBindViewHolder(holder: PaymentViewHolder, position: Int) {
-        // Bind data to the ViewHolder
         val currentPayment = payments[position]
         holder.descriptionTextView.text = currentPayment.description
         holder.amountTextView.text = currentPayment.amount.toString()
+        holder.dateTextView.text = currentPayment.date.toString() // Assuming PaymentDetailsModel includes a 'date' field
+
+        // Set a long click listener for refund request
+        holder.itemView.setOnLongClickListener {
+            onRefundRequested(currentPayment)
+            true // Indicates the click was handled
+        }
     }
 
-    override fun getItemCount() = payments.size
+    override fun getItemCount(): Int = payments.size
 
     class PaymentViewHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
         val descriptionTextView: TextView = itemView.findViewById(R.id.descriptionTextView)
         val amountTextView: TextView = itemView.findViewById(R.id.amountTextView)
+        val dateTextView: TextView = itemView.findViewById(R.id.dateTextView) // Make sure this ID exists in item_payment.xml
     }
 }
