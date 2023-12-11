a6bb07c71dd6a68424f83cc1e3dea7e25b39998e
MelihSahinEdu <melihsahin21@ku.edu.tr>
2023-12-11 13:31:47 +0300

    new kotlin gradle

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,6 +3,7 @@ plugins {
     id("com.android.application") version "8.1.2" apply false
     id("org.jetbrains.kotlin.android") version "1.9.0" apply false
     id("com.google.gms.google-services") version "4.3.13" apply false  // Google Services plugin
+    id("org.jmailen.kotlinter") version "4.1.0"
 }
 
 
