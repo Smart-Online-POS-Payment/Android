0e0dfd3b620219e9ea50c8444404b4b220223576
esatpiskin <wraithrayy@protonmail.com>
2023-12-16 16:33:41 +0300

    Doesnt navigate to my profile but verify tckn added

--- app/src/main/java/com/example/myapplication/HomePageActivity.kt
+++ app/src/main/java/com/example/myapplication/HomePageActivity.kt
@@ -4,23 +4,13 @@ import android.annotation.SuppressLint
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import com.example.myapplication.databinding.ActivityHomePageBinding
 import com.google.firebase.auth.FirebaseAuth
-
-import android.view.MenuItem
-import android.view.View
-
-import android.widget.ImageView
-import android.widget.PopupMenu
-
-import androidx.core.view.GravityCompat
-import okhttp3.Call
-import okhttp3.Callback
-import okhttp3.OkHttpClient
-import okhttp3.Request
-import okhttp3.Response
+import okhttp3.*
 import org.json.JSONObject
+
 import java.io.IOException
 
 class HomePageActivity : AppCompatActivity() {
@@ -32,32 +22,23 @@ class HomePageActivity : AppCompatActivity() {
         binding = ActivityHomePageBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
         val currentUser = FirebaseAuth.getInstance().currentUser
         getBalance(currentUser!!.uid)
-        // Set OnClickListener for the QR ImageView
+
+        setupOnClickListeners()
+    }
+
+    private fun setupOnClickListeners() {
         binding.qrView.setOnClickListener {
-            onImageViewClick("QR ImageView")
-
-            val Intent= Intent(this, QRScannerActivity::class.java)
-            startActivity(Intent)
-
+            startActivity(Intent(this, QRScannerActivity::class.java))
         }
 
-        // Set OnClickListener for the Credit Card CardView
         binding.creditCardLayoutView.setOnClickListener {
-            onCardViewClick("Credit Card CardView")
-            val Intent= Intent(this, EnterAmountActivity::class.java)
-            startActivity(Intent)
+            startActivity(Intent(this, EnterAmountActivity::class.java))
         }
 
-        // Set OnClickListener for the Payment CardView
-
-        // Set OnClickListener for the Wealth CardView
         binding.paymentsView.setOnClickListener {
-            onCardViewClick("Payments CardView")
-            val intent = Intent(this, PaymentHistoryActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, PaymentHistoryActivity::class.java))
         }
 
         val profileIcon = findViewById<ImageView>(R.id.profileIcon)
@@ -66,19 +47,19 @@ class HomePageActivity : AppCompatActivity() {
             startActivity(intent)
         }
 
-
         binding.logoutView.setOnClickListener {
             logoutUser()
         }
     }
 
-    private fun getBalance(customerId: String){
+    private fun getBalance(customerId: String) {
         val client = OkHttpClient().newBuilder().build()
         val request = Request.Builder()
-            .url("http://192.168.128.54:8070/wallet/$customerId")
+            .url("http://192.168.1.107:8070/wallet/$customerId")
             .get()
             .addHeader("Content-Type", "application/json")
             .build()
+
         client.newCall(request).enqueue(object : Callback {
             @SuppressLint("SetTextI18n")
             override fun onResponse(call: Call, response: Response) {
@@ -88,40 +69,29 @@ class HomePageActivity : AppCompatActivity() {
                         try {
                             val jsonObject = JSONObject(responseBody.string())
                             val balance = jsonObject.getDouble("balance")
-                            Log.i("Balance:", balance.toString())
-                            binding.walletTextView.text = "Balance: $balance"
-
-                            // Now you can use the 'balance' variable as needed.
+                            runOnUiThread {
+                                binding.walletTextView.text = "Balance: $balance"
+                            }
                         } catch (e: Exception) {
                             Log.e("Error", "Failed to parse JSON", e)
                         }
                     }
                 } else {
-                    Log.e("Error", "Failed to get payments")
+                    Log.e("Error", "Failed to get balance")
                 }
             }
 
             override fun onFailure(call: Call, e: IOException) {
-                Log.e("Error", "Failed to get payments", e)
+                Log.e("Error", "Failed to get balance", e)
             }
         })
     }
 
-    private fun onCardViewClick(cardName: String) {
-        println("Clicked on $cardName")
-    }
-
-    private fun onImageViewClick(imageName: String) {
-        println("Clicked on $imageName")
-    }
     private fun logoutUser() {
-        // Sign out from Firebase
         FirebaseAuth.getInstance().signOut()
-
-        // Redirect to MainActivity (Sign-in Screen)
         val intent = Intent(this, MainActivity::class.java)
-        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK  // Clear the activity stack
+        intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
         startActivity(intent)
+        finish() // This line is added to close the current activity
     }
-
 }
--- app/src/main/java/com/example/myapplication/MyProfileActivity.kt
+++ app/src/main/java/com/example/myapplication/MyProfileActivity.kt
@@ -1,42 +1,175 @@
 package com.example.myapplication
 
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
 import android.widget.Button
 import android.widget.EditText
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.google.firebase.auth.FirebaseAuth
+import com.google.gson.Gson
+import okhttp3.*
+import okhttp3.MediaType.Companion.toMediaTypeOrNull
+import java.io.IOException
+import java.util.Calendar
 
 class MyProfileActivity : AppCompatActivity() {
-
+    private val tcknValue: Long = tcknEditText.text.toString().toLongOrNull() ?: 0L
+    private lateinit var tcknEditText: EditText
+    private lateinit var nameEditText: EditText
+    private lateinit var surnameEditText: EditText
+    private lateinit var dobEditText: EditText
+    private lateinit var phoneNumberEditText: EditText
+    private lateinit var openAddressEditText: EditText
+    private lateinit var cityEditText: EditText
+    private lateinit var verifyTcknButton: Button
+    private lateinit var emailEditText: EditText
+    private lateinit var passwordEditText: EditText
+    private lateinit var changePasswordButton: Button
+    private lateinit var backButton: Button
+    val currentUser = FirebaseAuth.getInstance().currentUser
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_my_profile)
 
-        val ssnEditText: EditText = findViewById(R.id.TCKNEditText)
-        val nameEditText: EditText = findViewById(R.id.nameEditText)
-        val surnameEditText: EditText = findViewById(R.id.surnameEditText)
-        val dobEditText: EditText = findViewById(R.id.dobEditText)
-        val emailEditText: EditText = findViewById(R.id.emailEditText)
-        val passwordEditText: EditText = findViewById(R.id.passwordEditText)
-        val changePasswordButton: Button = findViewById(R.id.changePasswordButton)
-        val backButton: Button = findViewById(R.id.backButton)
+        initializeViews()
+
+
+        emailEditText.setText(currentUser?.email)
+        passwordEditText.setText("********")
+
+        verifyTcknButton.setOnClickListener {
+            val tcknValue = tcknEditText.text.toString().toLongOrNull() ?: 0L
+            verifyTckn(tcknValue, nameEditText.text.toString(), surnameEditText.text.toString(), emailEditText.text.toString(),
+                dobEditText.text.toString(), phoneNumberEditText.text.toString(), openAddressEditText.text.toString(), cityEditText.text.toString())
+        }
+
+        changePasswordButton.setOnClickListener {
+            // Implementation for changing password
+        }
+
         backButton.setOnClickListener {
             finish() // This will close MyProfileActivity and return to HomePageActivity
         }
-        // Example: Set the email field with the current user's email
-        val currentUser = FirebaseAuth.getInstance().currentUser
-        emailEditText.setText(currentUser?.email)
-
-        // Set a dummy password text
-        passwordEditText.setText("********")
-
-        // Set OnClickListener for the Change Password button
-        changePasswordButton.setOnClickListener {
-            // To be implemented
-        }
-
-        // Load and display other user data like SSN, Name, etc.
-        // This data must be retrieved from your database or user management system
+    }
+    private fun initializeViews() {
+        tcknEditText = findViewById(R.id.TCKNEditText)
+        nameEditText = findViewById(R.id.nameEditText)
+        surnameEditText = findViewById(R.id.surnameEditText)
+        dobEditText = findViewById(R.id.dobEditText)
+        phoneNumberEditText = findViewById(R.id.phoneNumberEditText)
+        openAddressEditText = findViewById(R.id.openAddressEditText)
+        cityEditText = findViewById(R.id.cityEditText)
+        verifyTcknButton = findViewById(R.id.verifyTCKNButton)
+        emailEditText = findViewById(R.id.emailEditText)
+        passwordEditText = findViewById(R.id.passwordEditText)
+        changePasswordButton = findViewById(R.id.changePasswordButton)
+        backButton = findViewById(R.id.backButton)
     }
 
+
+    private fun verifyTckn(tckn: Long, name: String, surname: String, email: String, dob: String, phoneNumber: String, openAddress: String, city: String) {
+        val verifyRequest = TcknVerifyRequest(tckn, name, surname, email, dob, phoneNumber, openAddress, city)
+        sendVerificationRequest(verifyRequest)
+    }
+
+    private fun sendVerificationRequest(verifyRequest: TcknVerifyRequest) {
+        val gson = Gson()
+        val jsonRequest = gson.toJson(verifyRequest)
+        val requestBody = RequestBody.create("application/json; charset=utf-8".toMediaTypeOrNull(), jsonRequest)
+        val cuid = currentUser?.uid
+        val request = Request.Builder()
+            .url("http://172.20.18.2:8081/verify/customer/$cuid") // Replace with your verification endpoint
+            .post(requestBody)
+            .build()
+
+        OkHttpClient().newCall(request).enqueue(object : Callback {
+            override fun onFailure(call: Call, e: IOException) {
+                runOnUiThread {
+                    Toast.makeText(applicationContext, "Verification request failed: ${e.message}", Toast.LENGTH_LONG).show()
+                }
+            }
+
+            override fun onResponse(call: Call, response: Response) {
+                runOnUiThread {
+                    if (response.isSuccessful) {
+                        Toast.makeText(applicationContext, "Verification request sent successfully", Toast.LENGTH_LONG).show()
+                    } else {
+                        Toast.makeText(applicationContext, "Verification request failed: ${response.message}", Toast.LENGTH_LONG).show()
+                    }
+                }
+            }
+        })
+    }
+
+
+    private fun setupDateField() {
+        val setupDateField = findViewById<EditText>(R.id.editTextExpiryDate)
+        setupDateField.addTextChangedListener(object : TextWatcher {
+            private var current = ""
+            private val ddmmyyyy = "DDMMYYYY"
+            private val cal = Calendar.getInstance()
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                if (s.toString() != current) {
+                    var clean = s.toString().replace(Regex("[^\\d.]|\\."), "")
+                    val cleanC = current.replace(Regex("[^\\d.]|\\."), "")
+
+                    val cl = clean.length
+                    var sel = cl
+                    var i = 2
+                    while (i <= cl && i < 6) {
+                        sel++
+                        i += 2
+                    }
+                    //Fix for pressing delete next to a forward slash
+                    if (clean == cleanC) sel--
+
+                    if (clean.length < 8) {
+                        clean = clean + ddmmyyyy.substring(clean.length)
+                    } else {
+                        //This part makes sure that when we finish entering numbers
+                        //the date is correct, fixing it otherwise
+                        var day = Integer.parseInt(clean.substring(0, 2))
+                        var mon = Integer.parseInt(clean.substring(2, 4))
+                        var year = Integer.parseInt(clean.substring(4, 8))
+
+                        mon = if (mon < 1) 1 else if (mon > 12) 12 else mon
+                        cal.set(Calendar.MONTH, mon - 1)
+                        year = if (year < 2023) 2023 else if (year > 2100) 2100 else year
+                        cal.set(Calendar.YEAR, year)
+                        // ^ first set year for the line below to work correctly
+                        //with leap years - otherwise, it will crash
+                        day = if (day > cal.getActualMaximum(Calendar.DATE)) cal.getActualMaximum(Calendar.DATE) else day
+                        clean = String.format("%02d%02d%02d", day, mon, year)
+                    }
+
+                    clean = String.format("%s/%s/%s", clean.substring(0, 2),
+                        clean.substring(2, 4),
+                        clean.substring(4, 8))
+
+                    sel = if (sel < 0) 0 else sel
+                    current = clean
+                    setupDateField.setText(current)
+                    setupDateField.setSelection(if (sel < current.length) sel else current.length)
+                }
+            }
+
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {}
+
+            override fun afterTextChanged(s: Editable?) {}
+        })
+    }
 }
+
+data class TcknVerifyRequest(
+    val tckn: Long,
+    val firstname: String,
+    val surname: String,
+    val dob: String,
+    val email: String,
+    val phoneNumber: String,
+    val openAddress: String,
+    val city: String
+)
