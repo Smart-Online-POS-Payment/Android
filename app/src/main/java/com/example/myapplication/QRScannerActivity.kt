7bd0e5ae8977c4058975159c0a987f9069384eb6
KaanSimsek19 <kaan.simsek@valensas.com>
2024-01-12 00:35:43 +0300

    fix bug

--- app/src/main/java/com/example/myapplication/QRScannerActivity.kt
+++ app/src/main/java/com/example/myapplication/QRScannerActivity.kt
@@ -131,7 +131,7 @@ class QRScannerActivity: AppCompatActivity() {
     private fun getPaymentRequest(qr: String, accessToken: String, uid: String): ResponseBody? {
         val client = OkHttpClient().newBuilder().build()
         val request = Request.Builder()
-            .url("${Constants.GATEWAY_URL}/payment/payment-request/$qr/customer/$uid")
+            .url("${Constants.PAYMENT_URL}/payment/payment-request/$qr/customer/$uid")
             .get()
             .addHeader("Authorization", "Bearer $accessToken")
             .addHeader("Content-Type", "application/json")
--- app/src/main/java/com/example/myapplication/util/Constants.kt
+++ app/src/main/java/com/example/myapplication/util/Constants.kt
@@ -2,4 +2,5 @@ package com.example.myapplication.util
 
 object Constants {
     const val GATEWAY_URL = "http://34.135.253.130:80"
+    const val PAYMENT_URL = "http://34.41.35.12:80"
 }
