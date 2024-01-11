03fac1e4ec3dc3e06260657b8458fccd354a1719
KaanSimsek19 <kaan.simsek@valensas.com>
2024-01-12 00:18:46 +0300

    add gateway url

--- app/src/main/java/com/example/myapplication/util/Constants.kt
+++ app/src/main/java/com/example/myapplication/util/Constants.kt
@@ -1,9 +1,5 @@
 package com.example.myapplication.util
 
 object Constants {
-    const val BASE_URL = "http://172.21.167.244"
-    const val AUTH_URL = "http://172.21.168.23:8081"
-    const val WALLET_URL = "http://172.21.168.23:8082"
-    const val PAYMENT_URL = "http://172.21.168.23:8083"
-    const val GATEWAY_URL = ""
+    const val GATEWAY_URL = "http://34.135.253.130:80"
 }
